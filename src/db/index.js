d0dad2cabef1647b5115f5725f498a0d466cfb62
19AbhishekSingh <109497501+19AbhishekSingh@users.noreply.github.com>
2024-07-26 21:29:10 +0530

    jwt tokens and file uploading

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,7 @@ import { DB_NAME } from '../constatnts.js';
 const connectDB = async() => {
     try{
 const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`); 
+console.log(connectionInstance)
 console.log(`connected DBHOST:${connectionInstance.connection.host}`)
     } catch(error){
         console.log("Error connecting",error)
